b8e60e4dfdd1ec5e20cc119035a488ff6b4ff732
aurelenc <aurele.nicolas@epitech.eu>
2024-08-11 20:02:28 +0200

    use switch case for debug tile changing

--- src/events/events_civ.c
+++ src/events/events_civ.c
@@ -25,16 +25,20 @@ int in_tile(tile_t *tile, sfVector2f *vectors, sfVector2f point, sfConvexShape *
     sfVector2f origin = sfConvexShape_getOrigin(shape);
     int point_count = sfConvexShape_getPointCount(shape);
     sfVector2f pos1 = {tile->pos.x - origin.x, tile->pos.y - origin.y};
-    sfVector2f pos2 = { \
+    sfVector2f pos2 = {
         tile->pos.x - origin.x + vectors[1].x, tile->pos.y - origin.y + vectors[1].y};
 
-    for (int i = 0; i < point_count; i++) {
+    for (int i = 0; i < point_count; i++)
+    {
         pos1.x = tile->pos.x - origin.x + vectors[i].x;
         pos1.y = tile->pos.y - origin.y + vectors[i].y;
-        if (i + 1 < point_count) {
+        if (i + 1 < point_count)
+        {
             pos2.x = tile->pos.x - origin.x + vectors[i + 1].x;
             pos2.y = tile->pos.y - origin.y + vectors[i + 1].y;
-        } else {
+        }
+        else
+        {
             pos2.x = tile->pos.x - origin.x + vectors[0].x;
             pos2.y = tile->pos.y - origin.y + vectors[0].y;
         }
@@ -49,24 +53,38 @@ void check_keyboard_inputs(civlike_t *civ, sfEvent event)
 {
     if (event.type != sfEvtKeyPressed)
         return;
-    if (event.key.code == sfKeyNumpad0)
+    switch (event.key.code)
+    {
+    case sfKeyNumpad0:
         civ->graph->tiles[civ->selected_tile]->type = 0;
-    else if (event.key.code == sfKeyNumpad1)
+        break;
+    case sfKeyNumpad1:
         civ->graph->tiles[civ->selected_tile]->type = 1;
-    else if (event.key.code == sfKeyNumpad2)
+        break;
+    case sfKeyNumpad2:
         civ->graph->tiles[civ->selected_tile]->type = 2;
-    else if (event.key.code == sfKeyNumpad3)
+        break;
+    case sfKeyNumpad3:
         civ->graph->tiles[civ->selected_tile]->type = 3;
-    else if (event.key.code == sfKeyNumpad4)
+        break;
+    case sfKeyNumpad4:
         civ->graph->tiles[civ->selected_tile]->type = 4;
-    else if (event.key.code == sfKeyNumpad5)
+        break;
+    case sfKeyNumpad5:
         civ->graph->tiles[civ->selected_tile]->type = 5;
-    else if (event.key.code == sfKeyNumpad6)
+        break;
+    case sfKeyNumpad6:
         civ->graph->tiles[civ->selected_tile]->type = 6;
-    else if (event.key.code == sfKeyNumpad7)
+        break;
+    case sfKeyNumpad7:
         civ->graph->tiles[civ->selected_tile]->type = 7;
-    else if (event.key.code == sfKeyNumpad8)
+        break;
+    case sfKeyNumpad8:
         civ->graph->tiles[civ->selected_tile]->type = 8;
+        break;
+    default:
+        break;
+    }
     save_map(civ);
 }
 
@@ -75,19 +93,24 @@ void check_mouse_events(civlike_t *civ, sfEvent event, sfVector2f mouse_rel)
     sfVector2f view_size = sfView_getSize(civ->graph->view);
 
     civ->graph->window_size = sfRenderWindow_getSize(civ->graph->window);
-    if (event.type == sfEvtMouseButtonPressed) {
-        for (int i = 0; i < MAX_TILES; i++) {
-            if (in_tile(civ->graph->tiles[i], civ->graph->hex_vectors, mouse_rel, civ->graph->hex)) {
+    if (event.type == sfEvtMouseButtonPressed)
+    {
+        for (int i = 0; i < MAX_TILES; i++)
+        {
+            if (in_tile(civ->graph->tiles[i], civ->graph->hex_vectors, mouse_rel, civ->graph->hex))
+            {
                 civ->graph->tiles[i]->selected = true;
                 civ->selected_tile = i;
-            } else
+            }
+            else
                 civ->graph->tiles[i]->selected = false;
         }
     }
-    if (event.type == sfEvtMouseWheelScrolled) {
+    if (event.type == sfEvtMouseWheelScrolled)
+    {
         if (event.mouseWheelScroll.delta > 0 && view_size.y > (civ->graph->window_size.y * pow(0.8, 4)))
             sfView_zoom(civ->graph->view, 0.8);
-        else if (event.mouseWheelScroll.delta < 0 && view_size.y <  (civ->graph->window_size.y * pow(1.2, 4)))
+        else if (event.mouseWheelScroll.delta < 0 && view_size.y < (civ->graph->window_size.y * pow(1.2, 4)))
             sfView_zoom(civ->graph->view, 1.2);
         view_size = sfView_getSize(civ->graph->view);
         if (view_size.y < (civ->graph->window_size.y * pow(0.8, 4)))
